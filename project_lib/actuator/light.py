a62c559533e2be707ecb1157c19cc9fe219736b0
Etyenne <33944283+Etyenne@users.noreply.github.com>
2019-12-31 17:35:14 +0100

    Update light.py

--- project_lib/actuator/light.py
+++ project_lib/actuator/light.py
@@ -152,37 +152,37 @@ class LDR:
     def getOnOffState(self):
         return self.powerState
 
+if False:
+    # setup the pin name for ease of use
+    LDR_TRANSISTOR = 17
+    LDR_SENSOR = 27
 
-# setup the pin name for ease of use
-LDR_TRANSISTOR = 17
-LDR_SENSOR = 27
+    # setup the enumeration based on gpio references 
+    GPIO.setmode(GPIO.BCM)
 
-# setup the enumeration based on gpio references 
-GPIO.setmode(GPIO.BCM)
+    # setup the port as output or input
 
-# setup the port as output or input
-
-# setup the state of the port
+    # setup the state of the port
 
 
-# code of protection; if has no problem the block try will keep always on
-try:
-    capteurLDR_1 = LDR(LDR_TRANSISTOR, LDR_SENSOR)
-    while True:
-        capteurLDR_1.turnOnOff()
-        luminosityValue = capteurLDR_1.getLuminosity()
-        if luminosityValue == True:
-            print "light detected " 
-            time.sleep(1)
-        elif luminosityValue == False:
-            print "light not detected " 
-            time.sleep(1)
-        else:
-            print "sensor poweroff" 
-            time.sleep(1)
+    # code of protection; if has no problem the block try will keep always on
+    try:
+        capteurLDR_1 = LDR(LDR_TRANSISTOR, LDR_SENSOR)
+        while True:
+            capteurLDR_1.turnOnOff()
+            luminosityValue = capteurLDR_1.getLuminosity()
+            if luminosityValue == True:
+                print "light detected " 
+                time.sleep(1)
+            elif luminosityValue == False:
+                print "light not detected " 
+                time.sleep(1)
+            else:
+                print "sensor poweroff" 
+                time.sleep(1)
 
-# exceptions are anything that interrupt the try block.
-# if a CTRL_C be pressed
-except KeyboardInterrupt:
-# setup the gpio to default values; finish any transmission of energy
-    GPIO.cleanup()
+    # exceptions are anything that interrupt the try block.
+    # if a CTRL_C be pressed
+    except KeyboardInterrupt:
+    # setup the gpio to default values; finish any transmission of energy
+        GPIO.cleanup()
